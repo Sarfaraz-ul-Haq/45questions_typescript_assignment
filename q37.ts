110af4c8c0be643bca5b5ae6d46c3072c97c7d0f
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-02-01 15:15:46 +0500

    removed uneccessary + arrow func

--- q37.ts
+++ q37.ts
@@ -1,9 +1,6 @@
 // Large Shirts
 
-const make_shirt = (
-  size: string = "large",
-  text: string = "I love TypeScript"
-): void => {
+const make_shirt = (size = "large", text = "I love TypeScript"): void => {
   console.log(
     `The size of the shirt is ${size} and the text printed on it is ${text}`
   );
