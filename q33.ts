a4d3e16a2f9810cacea041b8eeb609d3ebc5c63a
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-02-01 15:05:27 +0500

    replaced let with const

--- q33.ts
+++ q33.ts
@@ -1,6 +1,6 @@
-//Ordinal Numbers
+// Ordinal Numbers
 
-let numArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let i = 0; i < numArr.length; i++) {
   if (numArr[i] === 1) {
