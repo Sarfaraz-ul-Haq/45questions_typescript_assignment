60dc6513b00867153b7ae806763dbc3e6ed61f53
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-02-03 11:48:23 +0500

    param name change

--- q44.ts
+++ q44.ts
@@ -4,11 +4,11 @@ import chalk from "chalk";
 
 // Write a function that accepts an array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
 
-const order_sandwich = (...array: string[]) => {
+const order_sandwich = (...items: string[]) => {
   console.log(chalk.green("Summary of sandwich:"));
 
-  for (let i = 0; i < array.length; i++) {
-    console.log(chalk.gray(`- ${array[i]}`));
+  for (let i = 0; i < items.length; i++) {
+    console.log(chalk.gray(`- ${items[i]}`));
   }
 };
 
