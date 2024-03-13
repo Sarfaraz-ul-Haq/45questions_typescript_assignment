3523ebefedbea51999439626ffbbb4af66ca744c
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-13 22:52:45 +0500

    replaced arrow function with regular function

--- q45.ts
+++ q45.ts
@@ -2,11 +2,11 @@
 
 // Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
 
-const carInfoFunc = (
+function carInfoFunc(
   manufacturer: string,
   model_name: string,
   keywordArguments: { [key: string]: any }
-) => {
+) {
   let carInfo = {
     manufacturer: manufacturer,
     model_name: model_name,
@@ -14,7 +14,7 @@ const carInfoFunc = (
   };
 
   return carInfo;
-};
+}
 
 const info = carInfoFunc("BMW", "BMW i7", {
   color: "Black",
